cefed7f114d742d91cb7779115c6c6cc90c31e8b
Balint Cristian <cristian.balint@gmail.com>
2022-02-01 12:04:44 +0200

    Add tag versioning rule exception for bladerf.

--- update-copr-pkgs.py
+++ update-copr-pkgs.py
@@ -147,6 +147,7 @@ def gitCheckVersion(pkgname, branch, screpo, dover = False):
       # blacklists
       if ("gcc" in pkgname): continue
       if ("gklib" in pkgname): continue
+      if ("bladerf" in pkgname and "_" in vers): continue
       if ("limesuite" in pkgname and "-" in vers): continue
       if ("onednn" in pkgname and "graph" in vers): continue
       if ("gdb" in pkgname and "binutils" in vers): continue
