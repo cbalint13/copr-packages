4daf9af47465f84ddef7cd1c3be7f4261121f8d1
Balint Cristian <cristian.balint@gmail.com>
2022-05-24 23:41:11 +0300

    Add tensorflow n-v-r to blacklist.

--- copr-pkgs-update.py
+++ copr-pkgs-update.py
@@ -215,6 +215,7 @@ def gitCheckVersion(pkgname, branch, screpo, dover = False):
       if ("gcc" in pkgname): continue
       if ("gklib" in pkgname): continue
       if ("nextpnr" in pkgname): continue
+      if ("tensorflow" in pkgname): continue
       if ("bladerf" in pkgname and "_" in vers): continue
       if ("limesuite" in pkgname and "-" in vers): continue
       if ("onednn" in pkgname and "graph" in vers): continue
